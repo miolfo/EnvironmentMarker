7e6db4b9b8d96a7529cd5f1507929e5037342e0b
miolfo <mikko.o.forsman@gmail.com>
2021-01-21 19:09:46 +0200

    Add banner to configured domains

--- src/js/marker.js
+++ src/js/marker.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+function addBanner(message, type) {
+    const className = "envmarker-banner " + type
+    const existingBanner = document.getElementsByClassName(className)
+    //Don't add a new banner if one already exists
+    if(existingBanner.length === 0) {
+        const element = document.createElement("div")
+        element.setAttribute("class", className)
+        element.innerText = message
+        document.body.prepend(element)
+    }
+}
+
+browser.storage.local.get().then((res) => {
+    let bannerAdded = false
+    res.markers.forEach(element => {
+        const host = window.location.hostname.replace("www.", "").toLowerCase()
+        if(!bannerAdded && host === element.domain.toLowerCase()) {
+            bannerAdded = true
+            addBanner(element.message, element.type)
+        }
+    });
+})
+
